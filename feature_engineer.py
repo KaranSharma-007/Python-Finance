ada6f2d3ed299c19539ec6618401045a124e2911
KaranSharma-007 <karan.sharma@audencia.com>
2025-03-27 23:21:32 +0100

    Improved performance with new features

--- feature_engineer.py
+++ feature_engineer.py
@@ -10,14 +10,13 @@ def add_financial_ratios(financials_data):
     Returns:
         dataframe: finacial dataframe with features added
     """
-    financials_data['Operating_Margin'] = financials_data['ProfitBeforeTax'] / financials_data['Total_Revenue']
+    financials_data['Operating_Margin'] = round(financials_data['ProfitBeforeTax'] / financials_data['Total_Revenue'], 2)
     financials_data['EBITDA'] = financials_data['ProfitBeforeTax'] + financials_data['FinanceCosts'] + financials_data['DepreciationDepletionAndAmortisationExpense']
-    financials_data['EBITDA_Margin'] = financials_data['EBITDA'] / financials_data['Total_Revenue']
-    financials_data['Profit_Margin'] = financials_data['ProfitBeforeTax'] / financials_data['Total_Revenue']
-    financials_data['Revenue_Contribution'] = financials_data['RevenueFromOperations'] / financials_data['Total_Revenue']
-    financials_data['Expense_Ratio'] = financials_data['Total_Expenses'] / financials_data['Total_Revenue']
-    financials_data['Depreciation_Ratio'] = financials_data['DepreciationDepletionAndAmortisationExpense'] / financials_data['Total_Revenue']
-    financials_data['FinanceCost_Ratio'] = financials_data['FinanceCosts'] / financials_data['Total_Revenue']
+    financials_data['EBITDA_Margin'] = round(financials_data['EBITDA'] / financials_data['Total_Revenue'], 2)
+    financials_data['Profit_Margin'] = round(financials_data['ProfitBeforeTax'] / financials_data['Total_Revenue'], 2)
+    financials_data['Revenue_Contribution'] = round(financials_data['RevenueFromOperations'] / financials_data['Total_Revenue'], 2)
+    financials_data['Expense_Ratio'] = round(financials_data['Total_Expenses'] / financials_data['Total_Revenue'], 2)
+    financials_data['Depreciation_Ratio'] = round(financials_data['DepreciationDepletionAndAmortisationExpense'] / financials_data['Total_Revenue'], 2)
     return financials_data
 
 def add_price_features(stock_data):
@@ -30,19 +29,27 @@ def add_price_features(stock_data):
         dataframe: stock price dataframe with features added
     """
     stock_data = stock_data.sort_values('Date')
-    stock_data['Daily_Return'] = stock_data['Close'].pct_change()
+
+    # Convert to int
+    stock_data['Close'] = stock_data['Close']//1
+    stock_data['High'] = stock_data['High']//1
+    stock_data['Low'] = stock_data['Low']//1
+    stock_data['Open'] = stock_data['Open']//1
+
+    stock_data['Daily_Return'] = round(stock_data['Close'].pct_change()*100, 2)
     stock_data['Intraday_Range'] = stock_data['High'] - stock_data['Low']
-    stock_data['Close/Open'] = stock_data['Close'] / stock_data['Open']
-    stock_data['High/Low'] = stock_data['High'] / stock_data['Low']
+    stock_data['Close/Open'] = round(stock_data['Close'] / stock_data['Open'], 2)
+    stock_data['High/Low'] = round(stock_data['High'] / stock_data['Low'], 2)
     stock_data['Direction'] = np.where(stock_data['Close'] > stock_data['Open'], 1, 0)
 
-    stock_data['SMA_10'] = stock_data['Close'].rolling(10).mean()
-    stock_data['SMA_30'] = stock_data['Close'].rolling(30).mean()
+    stock_data['SMA_10'] = round(stock_data['Close'].rolling(10).mean() ,2)
+    stock_data['SMA_30'] = round(stock_data['Close'].rolling(30).mean(), 2)
 
-    stock_data['Volatility_10'] = stock_data['Daily_Return'].rolling(10).std()
-    stock_data['Momentum_5D'] = stock_data['Close'] - stock_data['Close'].shift(5)
+    stock_data['Volatility_10'] = round(stock_data['Daily_Return'].rolling(10).std(), 2)
+    stock_data['Momentum_5D'] = round(stock_data['Close'] - stock_data['Close'].shift(5), 2)
 
-    stock_data['Volume_Avg_10'] = stock_data['Volume'].rolling(10).mean()
+    stock_data['Volume_Avg_10'] = round(stock_data['Volume'].rolling(10).mean(), 2)
 
-    stock_data['RSI'] = ta.rsi(stock_data['Close'], length=14)
+    stock_data['RSI'] = round(ta.rsi(stock_data['Close'], length=14), 2)
+
     return stock_data
